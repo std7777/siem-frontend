2e07758850a7f0421586e04327ab73bed1bb3f05
Priyanshi <priyanshiv8033@gmail.com>
2026-04-09 17:30:02 +0530

    toast changed to class

--- src/components/ToastContainer.jsx
+++ src/components/ToastContainer.jsx
@@ -1,16 +1,39 @@
-import React from "react";
+import React, { Component } from "react";
 
-function ToastContainer({ toasts }) {
-  return (
-    <div className="toast-container">
-      {toasts.map((t) => (
-        <div className="toast" key={t.id}>
-          <span>{t.msg}</span>
-        </div>
-      ))}
-    </div>
-  );
+class ToastContainer extends Component {
+  constructor(props) {
+    super(props);
+    console.log("ToastContainer constructor", props);
+  }
+
+  componentDidMount() {
+    console.log("ToastContainer componentDidMount");
+  }
+
+  componentDidUpdate(prevProps) {
+    console.log("ToastContainer componentDidUpdate", {
+      previousToasts: prevProps.toasts,
+      currentToasts: this.props.toasts,
+    });
+  }
+
+  componentWillUnmount() {
+    console.log("ToastContainer componentWillUnmount");
+  }
+
+  render() {
+    const { toasts } = this.props;
+
+    return (
+      <div className="toast-container">
+        {toasts.map((t) => (
+          <div className="toast" key={t.id}>
+            <span>{t.msg}</span>
+          </div>
+        ))}
+      </div>
+    );
+  }
 }
 
 export default ToastContainer;
-
